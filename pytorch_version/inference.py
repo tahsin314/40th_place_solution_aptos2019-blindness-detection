f9f49fda8407b1701f1cd1d0d8b09e6c7d804625
tahsin314 <adarswapnik3.14@gmail.com>
2019-09-13 01:50:43 +0600

    Updated README and inference.py

--- pytorch_version/inference.py
+++ pytorch_version/inference.py
@@ -56,16 +56,16 @@ coef = [0.5, 1.5, 2.5, 3.5]
 for i, pred in enumerate(test_preds):
     if pred < coef[0]:
         test_preds[i] = 0
-    elif pred >= coef[0] and pred < coef[1]:
+    elif coef[0] <= pred < coef[1]:
         test_preds[i] = 1
-    elif pred >= coef[1] and pred < coef[2]:
+    elif coef[1] <= pred < coef[2]:
         test_preds[i] = 2
-    elif pred >= coef[2] and pred < coef[3]:
+    elif coef[2] <= pred < coef[3]:
         test_preds[i] = 3
     else:
         test_preds[i] = 4
 
 
-sample = pd.read_csv("data/sample_submission.csv")
+sample = pd.read_csv("data/new_data/sample_submission.csv")
 sample.diagnosis = test_preds.astype(int)
 sample.to_csv("submission.csv", index=False)
